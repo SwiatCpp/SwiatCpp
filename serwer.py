65d6bf8695d766d6f293fa4fe1fa6ec844559c8f
SwiatCpp <martacraft@onet.pl>
2025-10-11 20:18:34 +0200

    Add files via upload

--- serwer.py
+++ serwer.py
@@ -1,38 +1,41 @@
-from flask import Flask, request, render_template_string, send_file, redirect, url_for
+from flask import Flask, request, send_file
 
 app = Flask(__name__)
 
-# Strona główna z formularzem skargi
 @app.route('/')
 def home():
-    return '''
-    <h1>Świat C++</h1>
-    <p>Masz problem z programem? Napisz skargę!</p>
-    <form action="/skarga" method="post">
-        <input type="text" name="tresc" placeholder="Napisz swoją skargę" required>
-        <button type="submit">Wyślij skargę</button>
-    </form>
-    <p><a href="/pobierz_skargi">Pobierz wszystkie skargi</a></p>
-    '''
+    return send_file('index.html')
 
-# Odbieranie formularza i zapisywanie do pliku
-@app.route('/skarga', methods=['POST'])
-def skarga():
+@app.route('/programy.html')
+def programy():
+    return send_file('programy.html')
+
+@app.route('/starsze.html')
+def starsze():
+    return send_file('starsze.html')
+
+@app.route('/skargi_form')
+def skargi_form_get():
+    return send_file('skargi_form.html')
+
+@app.route('/skargi_form', methods=['POST'])
+def skargi_form_post():
     tresc = request.form['tresc']
     with open('skargi.txt', 'a', encoding='utf-8') as f:
         f.write(tresc + '\n')
     return '''
     <p>Dziękujemy za zgłoszenie!</p>
-    <p><a href="/">Wróć do strony głównej</a></p>
+    <button onclick="window.location.href='programy.html'">Wróć do Programy do pobrania</button>
+    <br>
+    <button onclick="window.location.href='index.html'">Wróć do strony głównej</button>
     '''
 
-# Pobieranie pliku skargi.txt
-@app.route('/pobierz_skargi')
+@app.route('/sekretny_link_do_skarg_123abc')
 def pobierz_skargi():
     try:
         return send_file('skargi.txt', as_attachment=True)
     except Exception:
-        return '<p>Plik skargi.txt jeszcze nie istnieje.</p><p><a href="/">Wróć</a></p>'
+        return '<p>Plik skargi.txt jeszcze nie istnieje.</p>'
 
 if __name__ == '__main__':
     app.run(debug=False)
